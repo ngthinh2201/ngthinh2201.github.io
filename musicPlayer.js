ea8215016e9579c2766050a491cdc8f8da3d17b4
ngthinh2201 <160304154+ngthinh2201@users.noreply.github.com>
2025-02-09 23:41:34 +0700

    Update musicPlayer.js

--- musicPlayer.js
+++ musicPlayer.js
@@ -9,12 +9,9 @@ document.addEventListener("DOMContentLoaded", function() {
             console.log("Error playing music:", error);
         });
     };
-
-    // Yêu cầu người dùng click để kích hoạt autoplay
     document.addEventListener('click', function() {
-        // Phát nhạc ngay lập tức
         playMusic();
-        // Phát nhạc sau 5 giây
+        // start 2 second
         setTimeout(playMusic, 2000);
     }, { once: true });
 });
